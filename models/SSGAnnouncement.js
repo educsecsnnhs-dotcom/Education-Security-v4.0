e2a2e48ad024e81ef185999e0cb1b226b763afe1
educsecsnnhs-dotcom <educsecsnnhs@gmail.com>
2025-09-17 01:23:23 +0800

    Update SSGAnnouncement.js

--- models/SSGAnnouncement.js
+++ models/SSGAnnouncement.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 const AnnouncementSchema = new mongoose.Schema({
   title: { type: String, required: true },
   body: { type: String, required: true },
-  createdBy: { type: String, required: true }, // username or userId
-  createdAt: { type: Date, default: Date.now }
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // userId as ObjectId
+  createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('SSGAnnouncement', AnnouncementSchema);
