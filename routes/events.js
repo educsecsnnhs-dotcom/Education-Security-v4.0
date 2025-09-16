8fcc1d41261b646928112f029e4980fab96f9a9e
educsecsnnhs-dotcom <educsecsnnhs@gmail.com>
2025-09-16 22:24:56 +0800

    Update events.js

--- routes/events.js
+++ routes/events.js
@@ -5,6 +5,6 @@ const comm = require("../controllers/communicationController");
 const { authRequired, requireAnyRole } = require("../middleware/authMiddleware");
 
 router.post("/", authRequired, requireAnyRole(["Admin", "Registrar", "SuperAdmin"]), comm.createEvent);
-router.get("/", comm.getAnnouncements); // reuse or create a getEvents in comm controller if preferred
+router.get("/", comm.getAnnouncements); // reuse or create getEvents in comm controller
 
 module.exports = router;
