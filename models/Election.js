f8b035a4ea3c7fcdc2e0b26e0dab08c1fab5b679
educsecsnnhs-dotcom <educsecsnnhs@gmail.com>
2025-09-17 01:19:58 +0800

    Update Election.js

--- models/Election.js
+++ models/Election.js
@@ -1,10 +1,13 @@
 // models/Election.js
 const mongoose = require('mongoose');
 
-const ElectionSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String },
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-}, { timestamps: true });
+const ElectionSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true }, // Title of the election
+    description: { type: String }, // Optional description of the election
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Creator (SuperAdmin, Admin, etc.)
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Election', ElectionSchema);
