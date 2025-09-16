0300cb096f0d77d460193cea88dcf812a696c6c2
educsecsnnhs-dotcom <educsecsnnhs@gmail.com>
2025-09-16 22:23:53 +0800

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,9 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 
 router.post("/register", authController.register);
-router.post("/login", authController.login);
-router.post("/logout", authController.logout);
-router.get("/me", authController.me);
+router.post("/login", authController.login);   // issues JWT
+router.post("/logout", authController.logout); // client just discards token
+router.get("/me", authController.me);          // checks JWT
 
 module.exports = router;
+
